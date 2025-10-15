f2b2de44b3da51856733f9530133b7575a0a2958
Wenzheng Cai <wenzhengcai@Wenzhengs-Air.fritz.box>
2025-10-15 10:58:58 +0200

    created class: Book and Author

--- data_models.py
+++ data_models.py
@@ -2,3 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Create the SQLAlchemy database instance
 db = SQLAlchemy()
+
+# Author model
+class Author(db.Model):
+    __tablename__ = 'authors'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    # Relationship to books
+    books = db.relationship('Book', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"<Author {self.id}: {self.name}>"
+
+    def __str__(self):
+        return f"Author: {self.name} (born {self.birth_date}, died {self.date_of_death})"
+
+
+# Book model
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(50), unique=True ,nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=True)
+
+    # Foreign Key
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Book {self.id}: {self.title}>"
+
+    def __str__(self):
+        return f"Book: {self.title} (published {self.publication_year}), ISBN: {self.isbn}"
