67dc1ee6b6f629ac13486725d7255fa4277337ce
Wenzheng Cai <wenzhengcai@Wenzhengs-Air.fritz.box>
2025-10-15 10:57:11 +0200

    Created table books and authors

--- app.py
+++ app.py
@@ -11,3 +11,7 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 db.init_app(app)
+
+# Create the tables, only run once!
+with app.app_context():
+    db.create_all()
