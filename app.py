62847ba19d8c7d0bb3b9bac2b5916a3689785830
Wenzheng Cai <wenzhengcai@Wenzhengs-Air.fritz.box>
2025-10-15 11:09:07 +0200

    added route:/add_author

--- app.py
+++ app.py
@@ -12,6 +12,31 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 db.init_app(app)
 
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    message = ""
+
+    if request.method == 'POST':
+        author_name = request.form.get('name')
+        birth_date = request.form.get('birthdate')
+        date_of_death = request.form.get('date_of_death')
+
+        if not author_name:
+            message = "Please enter a author name."
+        else:
+            try:
+                new_author = Author(name=author_name, birth_date=birth_date, date_of_death=date_of_death or None)
+                db.session.add(new_author)
+                db.session.commit()
+                message = f"Author {author_name} added successfully!"
+            except Exception as e:
+                db.session.rollback()
+                message = f"An error occurred while adding author {author_name} : {e}"
+
+    return render_template('add_author.html', message=message)
+
+
 # Create the tables, only run once!
-with app.app_context():
-    db.create_all()
+#with app.app_context():
+#    db.create_all()
