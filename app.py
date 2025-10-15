b6beae91ba187b0b70e4dc271a5c767a6b3e5fab
Wenzheng Cai <wenzhengcai@Wenzhengs-Air.fritz.box>
2025-10-15 10:50:42 +0200

    Configurated app.py

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+from data_models import db, Author, Book
+
+# Create the app
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+db.init_app(app)
